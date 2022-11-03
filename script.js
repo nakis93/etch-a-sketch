8991bc09d631732322e6e7320e4803229cb94263
nakis93 <zjnakis@gmail.com>
2022-11-03 16:04:12 +0200

    created original files

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const squares = document.createElement("squares");
+container.appendChild(squares);
+
+for (let i = 0; i < 16; i++) {
+  //run the next loop in row direction
+  for (let i = 0; i < 16; i++) {
+    //create 16 quares in column direction
+  }
+}

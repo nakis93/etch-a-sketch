8bab273c66bb2561929cd85bd0ac5e5c7f17ed16
nakis93 <zjnakis@gmail.com>
2022-11-04 11:41:11 +0200

    minor first changes

--- script.js
+++ script.js
@@ -3,14 +3,12 @@
 const grid = document.createElement("div");
 grid.id = "grid";
 document.body.appendChild(grid);
-console.log(grid);
 
 function createColumn() {
   for (let i = 0; i < 16; i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
     grid.appendChild(square);
-    console.log(square);
   }
 }
 

74866a84266ecb5390b427b8cf5d78aaf9a4124c
nakis93 <zjnakis@gmail.com>
2022-11-04 13:11:24 +0200

    found out how to create grid using flexbox

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ grid.id = "grid";
 document.body.appendChild(grid);
 
 function createColumn() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
     grid.appendChild(square);
   }
 }
-
-for (let i = 0; i < 16; i++) {
-  createColumn();
-}
+createColumn();

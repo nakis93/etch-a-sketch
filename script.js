e9810f2eacf0767f44ec105f8b8acb46558e0e5e
nakis93 <zjnakis@gmail.com>
2022-11-04 17:12:06 +0200

    sf

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 "use strict";
 // A function to create X columns
 const container = document.querySelector("#container");
+
 function createBox() {
   for (let i = 0; i < 256; i++) {
-    const box = document.createElement("div");
+    let box = document.createElement("div");
+
     box.classList.add("box");
     container.appendChild(box);
-    // console.log(box);
   }
 }
 createBox();
+
+// box.addEventListener("mouseover", () => {
+//   document.getElementsByClassName("box").style.color = "black";
+// });
